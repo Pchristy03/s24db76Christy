ec6a7b57e99d937fb585516076e4acd37acaf239
Parker W Christy <pchristy03@gmail.com>
2024-04-16 21:28:44 -0500

    add details and create fig buttons to fig search page

--- models/fig.js
+++ models/fig.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 const figSchema = mongoose.Schema({
     name: String,
     size: String,
-    age: Number,
+    age: {
+        type: Number,
+        min: [0, "age must be greater than or equal to 0"],
+        max: [100, "Age must not be greater than 100"]
+    },
     is_poisonous: Boolean
 })
 
